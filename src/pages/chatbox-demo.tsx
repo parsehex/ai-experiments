2c2e9addeaaf99d3866a95636cb824eacfb30694
parsehex <1779937+parsehex@users.noreply.github.com>
2023-11-08 19:16:52 -0500

    typo

--- src/pages/chatbox-demo.tsx
+++ src/pages/chatbox-demo.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { ChatBox } from '@/components/ChatBox';
-import { Message } from '@/app/types';
+import { Message } from '@/lib/types';
 import { v4 as uuidv4 } from 'uuid';
 
 function ChatboxDemo() {
