d945bb40f0ddc36a52028e4bfd48d3a5493444ce
parsehex <1779937+parsehex@users.noreply.github.com>
2023-11-08 20:42:21 -0500

    put layout back

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,25 @@
 import '@/styles/globals.scss';
 import type { Metadata } from 'next';
-import RootLayout from '@/components/Layout';
+import { Inter } from 'next/font/google';
+import Layout from '@/components/Layout';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
 	title: 'AI Demos and Experiments',
 	description: 'Collection of prototypes that use AI',
 };
 
-export default RootLayout;
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<Layout {...metadata}>{children}</Layout>
+			</body>
+		</html>
+	);
+}
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,6 @@
-import { useEffect } from 'react';
-import * as ooba from '@/lib/ooba-api';
 import Header from './Header';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
-	useEffect(() => {
-		(window as any).ooba = ooba;
-	}, []);
 	return (
 		<>
 			<Header />
