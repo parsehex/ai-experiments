84287bc9b2a64a5c94a3793546aa41e97b3df988
parsehex <1779937+parsehex@users.noreply.github.com>
2023-11-14 11:39:37 -0500

    dont return empty api keys

--- src/app/api/api-keys/route.ts
+++ src/app/api/api-keys/route.ts
@@ -1,19 +1,12 @@
 import { NextResponse } from 'next/server';
 
-export async function GET(request: Request) {
-	console.log(
-		'api-keys.ts: GET request.url',
-		request.url,
-		'process.env.OPENAI_API_KEY',
-		process.env.OPENAI_API_KEY,
-		'process.env.GOOGLE_API_KEY',
-		process.env.GOOGLE_API_KEY,
-		'process.env.GOOGLE_CSE_ID',
-		process.env.GOOGLE_CSE_ID
-	);
-	return NextResponse.json({
-		OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-		GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
-		GOOGLE_CSE_ID: process.env.GOOGLE_CSE_ID,
-	});
+export async function GET() {
+	const obj: Record<string, string> = {};
+	if (process.env.OPENAI_API_KEY)
+		obj.OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+	if (process.env.GOOGLE_API_KEY)
+		obj.GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
+	if (process.env.GOOGLE_CSE_ID)
+		obj.GOOGLE_CSE_ID = process.env.GOOGLE_CSE_ID;
+	return NextResponse.json(obj);
 }
