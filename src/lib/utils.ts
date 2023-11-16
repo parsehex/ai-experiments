1411cd546d48cf7e81c9f84ef1d7a76daea1a357
parsehex <1779937+parsehex@users.noreply.github.com>
2023-11-16 18:58:18 -0500

    fix chunking on newline

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -15,29 +15,48 @@ export async function chunkText(
 	maxTokens: number,
 	overlap: number
 ): Promise<TextChunk[]> {
-	// TODO avoid splitting in the middle of a sentence/line
 	const originalContent = chunk.originalContent || (chunk.content as string);
 	const tokens = await encodeTokens(originalContent, model);
+
+	// Encode a newline to get its token ID
+	const newlineToken = (await encodeTokens('\n', model)).slice(-1)[0];
+
 	let chunks: TextChunk[] = [];
 	let start = 0;
 	let i = 0;
 
 	while (start < tokens.length) {
 		let end = Math.min(start + maxTokens, tokens.length);
+
+		// Adjust end to fall on a newline token
 		if (end < tokens.length) {
-			// Move the end back to achieve overlap
-			end -= overlap;
+			let newlineIndex = tokens.lastIndexOf(newlineToken, end - 1);
+			if (newlineIndex >= start) {
+				end = newlineIndex + 1;
+			}
 		}
-		const chunkTokens = tokens.slice(start, end);
-		const chunkText = await decodeTokens(chunkTokens, model);
-		chunks.push({
-			id: v4(),
-			title: `${chunk.title} (part ${i++})`,
-			content: chunkText,
-			originalContent,
-			metadata: {},
-		});
+
+		if (i > 0) {
+			// Find the first newline token before the start (within the overlap range)
+			let overlapStart = Math.max(0, start - overlap);
+			let newlineIndex = tokens.indexOf(newlineToken, overlapStart);
+			start = newlineIndex >= 0 && newlineIndex < start ? newlineIndex : start;
+		}
+
+		if (start < end) {
+			const chunkTokens = tokens.slice(start, end);
+			const chunkText = await decodeTokens(chunkTokens, model);
+			chunks.push({
+				id: v4(),
+				title: `${chunk.title} (part ${i++})`,
+				content: chunkText.trim(),
+				originalContent,
+				metadata: {},
+			});
+		}
+
 		start = end;
 	}
+
 	return chunks;
 }
