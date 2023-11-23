c282a3566a3da28b1e90bcf1290471329f17bf17
parsehex <1779937+parsehex@users.noreply.github.com>
2023-11-22 19:39:20 -0500

    reorder demos

--- src/demosList.ts
+++ src/demosList.ts
@@ -5,119 +5,53 @@ export type ItemProps = {
 	tags?: string[];
 };
 
-export const demos: ItemProps[] = [
-	{
-		title: 'Role Play',
-		description: 'Character-based chat/role play without LangChain',
-		href: '/role-play',
-		tags: ['Chat', 'Story', 'In Progress', 'LLM'],
-	},
-	{
-		title: 'Story Generator',
-		description: 'Generate a story using a character, setting, and plot',
-		href: '/story-generator',
-		tags: ['Story', 'LLM', 'In Progress', 'Memory'],
-	},
-	{
-		title: 'Thought Chain',
-		description: 'Use LLM to answer questions in a multi-step process',
-		href: '/thought-chain',
-		// inspiration: https://old.reddit.com/r/LocalLLaMA/comments/17fmhcb/
-		tags: ['LLM', 'In Progress'],
-	},
+const tools: ItemProps[] = [
 	{
 		title: 'Summarizer',
 		description: 'Summarize collections of text',
 		href: '/summarizer',
-		tags: ['LLM', 'In Progress'],
-	},
-	{
-		title: 'Conversational Summary Memory',
-		description: 'Conversational memory without LangChain',
-		href: '/conversational-summary',
-		tags: ['Chat', 'LLM', 'In Progress', 'Memory'],
-	},
-	{
-		title: 'Inner Monologue Chat',
-		description: 'Chat where AI considers how to respond',
-		href: '/inner-monologue',
-		tags: ['Chat', 'LLM', 'In Progress'],
-	},
-	{
-		title: 'Simple Chat',
-		description: 'Simple chat meant to be a guide for other demos',
-		href: '/simple-chat',
-		tags: ['Chat', 'LLM', 'In Progress'],
-	},
-	{
-		title: 'Redacter',
-		description: 'Paste any text and remove identifying information',
-		href: '/redacter',
-		tags: ['LLM'],
+		tags: ['LLM', 'Tool'],
 	},
 	{
 		title: 'Transcribe Audio',
 		description: 'Transcribe audio from file',
 		href: '/transcribe-audio',
-		tags: ['In Progress', 'Whisper'],
-	},
-	{
-		title: 'Vision',
-		description: 'Demo showing how to use vision models',
-		// href: '/vision',
-		tags: ['TODO'],
-	},
-	{
-		title: 'Text to Speech',
-		description: 'Text to speech utility',
-		// href: '/text-to-speech',
-		tags: ['TTS', 'TODO'],
-	},
-	{
-		title: 'Suggestions',
-		description:
-			'Demo to try out generating suggestions to use in chat or other demos',
-		// href: '/suggestions',
-		tags: ['LLM', 'TODO'],
-	},
-	{
-		title: 'LangChain Chat',
-		description: 'Prototype chat app using LangChain',
-		href: '/langchain-chat',
-		tags: ['Chat', 'LLM', 'TODO'],
+		tags: ['Whisper', 'Tool'],
 	},
 	{
 		title: 'Image Generator',
 		description: 'Interface to Stable Diffusion for image generation',
 		href: '/image-generator',
-		tags: ['In Progress', 'SD'],
+		tags: ['Imagen', 'Tool'],
 	},
 	{
-		title: 'Entity Memory',
-		description: 'Entity memory for LangChain',
-		href: '/entity-memory',
-		tags: ['Chat', 'LLM', 'TODO'],
+		title: 'Redacter',
+		description: 'Paste any text and remove identifying information',
+		href: '/redacter',
+		tags: ['LLM', 'Tool'],
 	},
 	{
-		title: 'Embeddings & Search',
-		description: 'Search for similar documents using embeddings in a directory',
-		// href: '/embeddings-search',
-		tags: ['LLM', 'TODO'],
+		title: 'Vision',
+		description: 'Demo showing how to use vision models',
+		// href: '/vision',
+		tags: ['Tool'],
 	},
-
+	{
+		title: 'Text to Speech',
+		description: 'Text to speech utility',
+		// href: '/text-to-speech',
+		tags: ['TTS', 'Tool'],
+	},
 	{
 		title: 'Document QA',
 		description:
 			'Answer questions about a document, demo using vector stores, etc',
 		// href: '/document-qa',
-		tags: ['LLM', 'TODO'],
+		tags: ['LLM', 'Tool'],
 	},
-	{
-		title: 'Tools',
-		description: 'Demonstrate giving the LLM tools to use',
-		// href: '/tools',
-		tags: ['LLM', 'TODO'],
-	},
+];
+
+const componentPrototypes: ItemProps[] = [
 	{
 		title: 'Chat Box',
 		description: 'Reusable component for displaying a chat',
@@ -134,6 +68,79 @@ export const demos: ItemProps[] = [
 		title: 'Model Status',
 		description: 'Reusable component for managing the loaded model',
 		href: '/model-status',
-		tags: ['Component'],
+		tags: ['Component', 'LLM'],
 	},
+	{
+		title: 'Suggestions',
+		description:
+			'Demo to try out generating suggestions to use in chat or other demos',
+		// href: '/suggestions',
+		tags: ['LLM', 'Component'],
+	},
+];
+
+export const demos: ItemProps[] = [
+	{
+		title: 'Role Play',
+		description: 'Character-based chat/role play without LangChain',
+		href: '/role-play',
+		tags: ['Chat', 'Story', 'LLM'],
+	},
+	{
+		title: 'Story Generator',
+		description: 'Generate a story using a character, setting, and plot',
+		href: '/story-generator',
+		tags: ['Story', 'LLM', 'Memory'],
+	},
+	{
+		title: 'Inner Monologue Chat',
+		description: 'Chat where AI considers how to respond',
+		href: '/inner-monologue',
+		tags: ['Chat', 'LLM', 'Imagen'],
+	},
+	{
+		// inspiration: https://old.reddit.com/r/LocalLLaMA/comments/17fmhcb/
+		title: 'Thought Chain',
+		description: 'Use LLM to answer questions in a multi-step process',
+		href: '/thought-chain',
+		tags: ['LLM'],
+	},
+	{
+		title: 'Conversational Summary Memory',
+		description: 'Conversational memory without LangChain',
+		href: '/conversational-summary',
+		tags: ['Chat', 'LLM', 'Memory'],
+	},
+	{
+		title: 'Simple Chat',
+		description: 'Simple chat meant to be a guide for other demos',
+		href: '/simple-chat',
+		tags: ['Chat', 'LLM'],
+	},
+	{
+		title: 'LangChain Chat',
+		description: 'Prototype chat app using LangChain',
+		href: '/langchain-chat',
+		tags: ['Chat', 'LLM', 'LangChain'],
+	},
+	{
+		title: 'Entity Memory',
+		description: 'Entity memory',
+		href: '/entity-memory',
+		tags: ['Chat', 'LLM', 'Memory', 'LangChain'],
+	},
+	{
+		title: 'Embeddings & Search',
+		description: 'Search for similar documents using embeddings in a directory',
+		// href: '/embeddings-search',
+		tags: ['LLM'],
+	},
+	{
+		title: 'Tools',
+		description: 'Demonstrate giving the LLM tools to use',
+		// href: '/tools',
+		tags: ['LLM'],
+	},
+	...tools,
+	...componentPrototypes,
 ];
