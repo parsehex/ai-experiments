6ba70eb373d98b4b130b3ab974029f2755771c46
parsehex <1779937+parsehex@users.noreply.github.com>
2023-12-14 10:55:38 -0500

    break up prompt-part utils

--- src/app/story-generator/prompts/index.ts
+++ src/app/story-generator/prompts/index.ts
@@ -1,62 +1,12 @@
 import { PromptPart, PromptPartResponse } from '@/lib/types/llm';
 import { Character, Plot, Action } from '../types';
-
-function CharacterString(chars: Character[], isThought = true) {
-	const mapFunc = (c: Character) => {
-		let str = `- ${c.name}`;
-		if (
-			c.description ||
-			c.state ||
-			c.objectives.longTerm ||
-			c.objectives.shortTerm
-		) {
-			str += ' ';
-		}
-		if (c.description) str += `- DESCRIPTION: ${c.description} `;
-		if (c.state) str += `- STATE: ${c.state} `;
-		if (isThought && c.objectives.longTerm)
-			str += `- LONG-TERM OBJECTIVE: ${c.objectives.longTerm} `;
-		if (isThought && c.objectives.shortTerm)
-			str += `- SHORT-TERM OBJECTIVE: ${c.objectives.shortTerm} `;
-		return str.trim();
-	};
-	return chars.map(mapFunc).join('\n');
-}
-function SettingString(plot: Plot) {
-	let settingStr = `SETTING: `;
-	if (plot.location) settingStr += `${plot.location}`;
-	if (plot.timePeriod) {
-		settingStr += plot.location ? ` in ` : 'Time Period - ';
-		settingStr += plot.timePeriod;
-	}
-	return settingStr.trim();
-}
-function PlotString(plot: Plot, isThought = true) {
-	let str = '';
-	if (isThought && plot.storyDescription)
-		str += `DESCRIPTION: ${plot.storyDescription}\n`;
-	if (plot.tone) str += `TONE: ${plot.tone}\n`;
-	if (plot.location || plot.timePeriod) str += `${SettingString(plot)}`;
-	return str;
-}
-function ActionsString(actions: Action[]) {
-	return actions
-		.map((a) => {
-			let str = '';
-			if (a.type === 'Narrative') str = a.str;
-			else if (a.type === 'Dialogue') str = `${a.characterName}: ${a.str}`;
-			return str;
-		})
-		.join('\n');
-}
-function hasPlot(plot: Plot) {
-	return (
-		!!plot.storyDescription ||
-		!!plot.tone ||
-		!!plot.location ||
-		!!plot.timePeriod
-	);
-}
+import {
+	CharacterString,
+	PlotString,
+	ActionsString,
+	SettingString,
+	hasPlot,
+} from './utils';
 
 export function genStoryDescription(
 	chars: Character[],
