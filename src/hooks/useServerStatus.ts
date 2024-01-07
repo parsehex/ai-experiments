1c39a0d54bf78a1fa917f91c3fc0bab64bd66a47
parsehex <1779937+parsehex@users.noreply.github.com>
2024-01-07 17:58:45 -0500

    Update useServerStatus.ts

--- src/hooks/useServerStatus.ts
+++ src/hooks/useServerStatus.ts
@@ -7,13 +7,53 @@ interface JSONObject {
 type JSONArray = JSONValue[];
 type JSONValue = string | number | boolean | null | JSONObject | JSONArray;
 
+type TsetStatus = (status: ServerStatus) => void;
+type TsetModelInfo = (modelInfo: ModelInfo | null) => void;
+type TsetModels = (models: string[]) => void;
 interface InitWSOptions {
 	type: AIType;
-	setStatus: (status: ServerStatus) => void;
-	setModelInfo: (modelInfo: ModelInfo | null) => void;
-	setModels: (models: string[]) => void;
+	setStatus: TsetStatus;
+	setModelInfo: TsetModelInfo;
+	setModels: TsetModels;
 	wsSend: (message: JSONValue) => void;
 }
+
+function handleMessage(
+	setModelInfo: TsetModelInfo,
+	setModels: TsetModels,
+	setStatus: TsetStatus,
+	e: MessageEvent
+) {
+	const message = JSON.parse(e.data);
+	const { type: messageType, data } = message;
+	const { model, loader_name } = JSON.parse(data) ?? {};
+
+	switch (messageType) {
+		case 'get_model':
+		case 'load_model':
+			setModelInfo({ model, loader_name });
+			setStatus(ServerStatus.ON_MODEL_LOADED);
+			break;
+		case 'model_unload':
+			setModelInfo(null);
+			setStatus(ServerStatus.ON_NO_MODEL);
+			break;
+		case 'list_models':
+			const models =
+				data?.models?.sort((a: string, b: string) =>
+					a.toLowerCase().localeCompare(b.toLowerCase())
+				) ?? [];
+			setModels(models);
+			break;
+		default:
+			console.error('Unknown message type:', message);
+			break;
+	}
+}
+
+let ws: WebSocket | null = null;
+let didConnect = false;
+let isReconnecting = false;
 
 function initWS({
 	type,
@@ -22,8 +62,27 @@ function initWS({
 	setModels,
 	wsSend,
 }: InitWSOptions) {
-	const ws = new WebSocket(`ws://localhost:5000/${type}/v1/ws`);
+	const reconnect = () => {
+		setTimeout(() => {
+			initWS({ type, setStatus, setModelInfo, setModels, wsSend });
+		}, 5000);
+	};
+
+	if (isReconnecting) return reconnect();
+	isReconnecting = true;
+
+	try {
+		ws = new WebSocket(`ws://localhost:5000/${type}/v1/ws`);
+	} catch (error) {
+		setStatus(ServerStatus.OFF);
+		isReconnecting = false;
+		if (!isReconnecting) reconnect();
+		return;
+	}
+
 	ws.onopen = () => {
+		didConnect = true;
+		isReconnecting = false;
 		setStatus(ServerStatus.ON_NO_MODEL);
 		try {
 			wsSend({ type: 'list_models' });
@@ -32,42 +91,30 @@ function initWS({
 			console.error('Failed to send message:', error);
 		}
 	};
-	ws.onmessage = (event) => {
-		// console.log('Message received:', event);
-		const message = JSON.parse(event.data);
-		const data = JSON.parse(message.data);
-		switch (message.type) {
-			case 'get_model':
-			case 'load_model':
-				setModelInfo({
-					model: data.model,
-					loader_name: data.loader_name,
-				});
-				setStatus(ServerStatus.ON_MODEL_LOADED);
-				break;
-			case 'model_unload':
-				setModelInfo(null);
-				setStatus(ServerStatus.ON_NO_MODEL);
-				break;
-			case 'list_models':
-				data.models.sort((a: string, b: string) =>
-					a.toLowerCase().localeCompare(b.toLowerCase())
-				);
-				setModels(data.models);
-				break;
-			default:
-				console.error('Unknown message type:', message);
-				break;
-		}
-	};
-	ws.onclose = () => {
+
+	ws.onmessage = handleMessage.bind(null, setModelInfo, setModels, setStatus);
+
+	ws.onerror = (error) => {
+		if (!isReconnecting) console.error('WebSocket error:', error);
 		setStatus(ServerStatus.OFF);
+		if (!isReconnecting) reconnect();
 	};
+
+	ws.onclose = (event) => {
+		isReconnecting = false;
+		didConnect = false;
+		setStatus(ServerStatus.OFF);
+		if (!isReconnecting || !didConnect) reconnect();
+	};
+
 	return ws;
 }
 
 export function useServerStatus(type: AIType) {
-	let ws: WebSocket | null = null;
+	const [status, setStatus] = useState<ServerStatus>(ServerStatus.OFF);
+	const [modelInfo, setModelInfo] = useState<ModelInfo | null>(null);
+	const [models, setModels] = useState<string[]>([]);
+
 	const wsSend = (message: JSONValue) => {
 		if (!ws || ws.readyState !== WebSocket.OPEN) return;
 		if (typeof message !== 'string') message = JSON.stringify(message);
@@ -77,10 +124,6 @@ export function useServerStatus(type: AIType) {
 			console.error('Failed to send message:', error);
 		}
 	};
-
-	const [status, setStatus] = useState<ServerStatus>(ServerStatus.OFF);
-	const [modelInfo, setModelInfo] = useState<ModelInfo | null>(null);
-	const [models, setModels] = useState<string[]>([]);
 
 	useEffect(() => {
 		if (
@@ -95,21 +138,20 @@ export function useServerStatus(type: AIType) {
 		const startWS = (lastTimeout?: number) => {
 			setTimeout(() => {
 				try {
-					ws = initWS({ type, setStatus, setModelInfo, setModels, wsSend });
+					initWS({ type, setStatus, setModelInfo, setModels, wsSend });
 				} catch (error) {
 					console.error('Failed to start WS:', error);
 					setStatus(ServerStatus.OFF);
-					setTimeout(
-						startWS,
-						lastTimeout ? Math.max(60, lastTimeout * 1.25) : 1000
-					);
+					setTimeout(startWS, lastTimeout || 1000);
 				}
 			}, 250);
 		};
-		const refreshModel = async () => {
+
+		const refreshModel = () => {
 			wsSend({ type: 'get_model' });
 		};
-		const refreshModels = async () => {
+
+		const refreshModels = () => {
 			wsSend({ type: 'list_models' });
 		};
 
