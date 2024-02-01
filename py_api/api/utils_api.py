0ae0a4a218bc91a2b4d920fcd6e86a91e7874a4b
parsehex <1779937+parsehex@users.noreply.github.com>
2024-02-01 14:27:04 -0500

    add media-to-wav utils route

--- py_api/api/utils_api.py
+++ py_api/api/utils_api.py
@@ -1,17 +1,20 @@
 # TODO
 # convert audio to wav
-#   maybe /convert-audio?
 #   pass format (if wav then pick some defaults like 16000 hz, 16 bit, mono)
 #   allow options for trimming, etc.
 # remove noise from audio (with facebook denoiser)
 
 import logging, os, time
+from typing import Any
+import magic
 import subprocess as sp
 from fastapi import FastAPI, HTTPException, UploadFile, File, Query
+from fastapi.responses import FileResponse
 from py_api.args import Args
 from py_api.models.utils_api import GetVRAMResponse
-from py_api.utils import audio
+from py_api.utils import audio, media
 
+mime = magic.Magic(mime=True)
 logger = logging.getLogger(__name__)
 
 def utils_api(app: FastAPI):
@@ -36,3 +39,30 @@ def utils_api(app: FastAPI):
 			total=mem_total / 1024,
 			free=mem_free / 1024
 		)
+
+	@app.post('/utils/v1/media-to-wav', tags=['utils'])
+	async def media_to_wav(
+		file: UploadFile = File(...),
+		trim_start: float = Query(None),
+		trim_end: float = Query(None),
+	):
+		"""
+		Convert media file to wav, for use with STT.
+		"""
+		# TODO flag for downsampling (for transcription) - call it 'sample_for_stt'?
+		if file.filename is None or file.filename == '':
+			raise HTTPException(
+				status_code=400, detail="No file provided"
+			)
+		file_path = os.path.join(
+			Args['stt_input_dir'], file.filename
+		)
+
+		with open(file_path, 'wb') as f:
+			f.write(file.file.read())
+
+		file_type = mime.from_file(file_path)
+		if 'audio/wav' not in file_type:
+			file_path = audio.convert_to_wav(file_path)
+		return FileResponse(file_path, media_type='audio/wav')
+
